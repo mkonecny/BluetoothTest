fb8f3fe49e81ecc8727afa076a63df0fd17e5502
hellomello13 <m16black100@gmail.com>
2014-07-09 14:18:16 -0700

    implemented working regular bluetooth scanner

--- app/src/main/java/com/bluetooth/test/bluetoothtest/MyActivity.java
+++ app/src/main/java/com/bluetooth/test/bluetoothtest/MyActivity.java
@@ -2,8 +2,12 @@ package com.bluetooth.test.bluetoothtest;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.ParcelUuid;
 import android.util.Log;
@@ -11,7 +15,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.bluetooth.le.*;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.UUID;
 
@@ -19,6 +27,7 @@ import java.util.UUID;
 public class MyActivity extends Activity {
 
     private static final String TAG = "MyActivity";
+    private static final int REQUEST_ENABLE_BT = 1;
     byte[] advertisingBytes = new byte[] { //TODO test data based on ibeacon demo
         (byte) 0x4c, (byte) 0x00,   // Apple manufacturer ID
         (byte) 0x02, (byte) 0x15,   // iBeacon advertisement identifier
@@ -28,24 +37,99 @@ public class MyActivity extends Activity {
         (byte) 0x00, (byte) 0x01,   // major: 1
         (byte) 0x00, (byte) 0x02 }; // minor: 2
 
-    BluetoothManager bluetoothManager;
+    //BluetoothManager bluetoothManager;
     BluetoothAdapter bluetoothAdapter;
-
     BluetoothLeAdvertiser bluetoothLeAdvertiser;
+
+    Button scanButton;
+    TextView bluetoothState;
+    ArrayAdapter<String> btArrayAdapter;
+    ListView listDevicesFound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
+        scanButton = (Button)findViewById(R.id.scan_button);
+        bluetoothState = (TextView)findViewById(R.id.bluetooth_state_text);
+        listDevicesFound = (ListView)findViewById(R.id.devicesfound);
+        btArrayAdapter = new ArrayAdapter<String>(MyActivity.this, android.R.layout.simple_list_item_1);
+        listDevicesFound.setAdapter(btArrayAdapter);
+
         initBluetooth();
+        checkBluetooth();
+        registerReceiver(ActionFoundReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+        registerReceiver(StateChangeReceiver, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(ActionFoundReceiver);
+    }
+
+    private void checkBluetooth() {
+        if(bluetoothAdapter == null) {
+            bluetoothState.setText("Bluetooth NOT supported");
+        } else {
+            if(bluetoothAdapter.isEnabled()) {
+                if (bluetoothAdapter.isDiscovering()) {
+                    bluetoothState.setText("Bluetooth is currently discovering...");
+                } else {
+                    bluetoothState.setText("Bluetooth is Enabled");
+                    scanButton.setEnabled(true);
+                }
+            } else {
+                bluetoothState.setText("Bluetooth is NOT Enabled");
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
     }
 
     private void initBluetooth() {
-        bluetoothManager = (BluetoothManager) this.getApplicationContext().getSystemService(Context.BLUETOOTH_SERVICE);
-        bluetoothAdapter = bluetoothManager.getAdapter();
+        //bluetoothManager = (BluetoothManager) this.getApplicationContext().getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         bluetoothLeAdvertiser = bluetoothAdapter.getBluetoothLeAdvertiser();
     }
+
+    public void handleStartScanClick(View view) {
+        btArrayAdapter.clear();
+        bluetoothAdapter.startDiscovery();
+        bluetoothState.setText("Bluetooth is currently discovering...");
+        scanButton.setEnabled(false);
+        //Toast.makeText(MyActivity.this, "Beginning Scan...", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_ENABLE_BT) {
+            checkBluetooth();
+        }
+    }
+
+    private final BroadcastReceiver ActionFoundReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                btArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                btArrayAdapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+    private final BroadcastReceiver StateChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+               checkBluetooth();
+            }
+        }
+    };
 
     public void handleStartClick(View view) {
         //System.out.println("button press registered");
@@ -58,6 +142,9 @@ public class MyActivity extends Activity {
     }
 
     private void startAdvertise() {
+        if(!bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.enable();
+        }
         AdvertisementData.Builder dataBuilder = new AdvertisementData.Builder();
         AdvertiseSettings.Builder settingsBuilder = new AdvertiseSettings.Builder();
 
@@ -69,8 +156,8 @@ public class MyActivity extends Activity {
         dataBuilder.setIncludeTxPowerLevel(false); //TODO possible necessity for nexus 5 advertising
 
         Toast.makeText(MyActivity.this, "setup complete", Toast.LENGTH_SHORT).show();
-        Log.e(TAG, "Configuration completed");
-        bluetoothLeAdvertiser.startAdvertising(settingsBuilder.build(), dataBuilder.build(), startCallback);
+        Log.d(TAG, "Configuration completed");
+        //bluetoothLeAdvertiser.startAdvertising(settingsBuilder.build(), dataBuilder.build(), startCallback);
     }
 
     private void stopAdvertise() {
